ad9acaba38633a8a601ccb00b392920a093e5b50
Christian Lee <54609773+CzJLee@users.noreply.github.com>
2021-04-30 14:14:31 -0700

    Create sin and coordinate split functions

--- functions.py
+++ functions.py
@@ -1,2 +1,37 @@
 import math
+import numpy as np
+import matplotlib.pyplot as plt
+import random
 
+# Generate list of x values
+mu = 0
+sigma = 2 * math.pi
+x = np.random.normal(mu, sigma, size=(10,))
+
+y = np.sin(x)
+
+data = np.column_stack((x, y))
+print(data)
+
+plt.scatter(x, y)
+# plt.show()
+
+def split_coordinates(a):
+	"""
+	Split a 2D Tensor of coordinate pairs into two vectors of x-coordinates and y-coordinates.
+	This is helpful for plotting x and y pairs.
+
+	Args:
+		a (array_like): Array to be reshaped
+
+	Returns:
+		x, y: Returns two vectors of x and y coordinate points
+	"""
+	x, y = np.reshape(np.hsplit(a, 2), (2, -1))
+	return x, y
+
+def sin_generator(n, mu = 0, sigma = 2 * math.pi):
+	x = np.random.normal(mu, sigma, size=(n,))
+	y = np.sin(x)
+
+	return np.column_stack((x, y))
